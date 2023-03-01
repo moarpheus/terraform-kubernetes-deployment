9cc635108848ed1fd1f15d42980119496a4dcb4a
Juris Gribuska <juris.gribuska@itv.com>
2023-03-01 09:09:17 +0000

    removed comment

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 data "aws_region" "current" {}
 
 resource "aws_vpc" "main" {
-  cidr_block       = var.cidr_block # Defining the CIDR block use 10.0.0.0/24 for demo
+  cidr_block       = var.cidr_block
   instance_tenancy = "default"
   tags             = merge(var.tags, {
     "Name" = "vpc-${var.name}-${data.aws_region.current.name}"
